37319a98e29acc0fab69cf807125385ffb427343
Rajat Bhageria <rajat1996@gmail.com>
2015-02-05 23:21:29 -0500

    So far..answering some questions

--- Homework1/src/AdjacencyMatrix.java
+++ Homework1/src/AdjacencyMatrix.java
@@ -1,3 +1,6 @@
+import java.util.HashSet;
+import java.util.Set;
+
 
 public class AdjacencyMatrix {
 	
@@ -35,9 +38,46 @@ public class AdjacencyMatrix {
 		return matrix[w][h];
 	}
 	
+	/**
+	 * @return The number of different edges in the graph 
+	 */
+	public int getNumberOfVertices(){
+		Set<Integer> set = new HashSet<Integer>();
+		for (int i = 0; i < matrix.length; i++){
+			for (int j = 0; j < matrix.length; j++){
+				
+				if (matrix[i][j]){
+					if (!set.contains(i)) set.add(i);
+					if (!set.contains(j)) set.add(j);
+				}
+			}
+		}
+		return set.size();
+	}
+	
+	
+	/**
+	 * @return Return Length/Height of Matrix
+	 */
 	public int getLength(){
 		return n;
 	}
+	
+	/**
+	 * @return Representation of the AdjacencyMatrix
+	 */
+	public String toString(){
+		String out = "";
+		for (int i = 0; i < matrix.length; i++){
+			for (int j = 0; j < matrix.length; j++){
+				out = out + " " + matrix[i][j] + " ";
+			}
+			out = out + "\n";
+		}
+		return out;
+	}
+	
+	
 	
 	
 }
--- Homework1/src/Main.java
+++ Homework1/src/Main.java
@@ -13,10 +13,12 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		AdjacencyMatrix matrix = new AdjacencyMatrix(4038);
+		//AdjacencyMatrix matrix = new AdjacencyMatrix(52);
+		AdjacencyMatrix matrix = new AdjacencyMatrix(4039);
 		int i = 0, j = 0;
 		
 		String file= "src/facebook_combined.txt";
+		//String file = "src/test1.txt";
 		try {
 		    BufferedReader in = new BufferedReader(new FileReader(file));
 		    String str;
@@ -34,13 +36,20 @@ public class Main {
 		    	}
 		    }
 		    in.close();
+		    //System.out.println(matrix.toString());
 		} catch (IOException e) {
 			System.out.println("ERROR: unable to read file " + file);
 		    e.printStackTrace(); 
 		}		
 		
 		Searches search = new Searches(matrix);
-		search.runBFS(0, 1);
+		//System.out.println(search.runBFS(0, 1232));
+		
+		System.out.println("The Distance between nodes 40 and 1050 is: " +search.runBFS(40, 1050));
+		//System.out.println("The Distance between nodes 40 and 1050 is: " +search.breadthFirstSearch(0, 51));
+
+
+		
 		
 	}
 
