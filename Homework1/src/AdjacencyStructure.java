a3ad5df6113e93060c00214d57735408fae515f8
Rajat Bhageria <rajat1996@gmail.com>
2015-02-06 22:35:10 -0500

    Commit...before bad stuff happens

---
+++ Homework1/src/AdjacencyStructure.java
@@ -0,0 +1,11 @@
+import java.util.Set;
+
+
+public interface AdjacencyStructure {
+	void addEdge(int i, int j);
+	boolean hasEdge (int i, int j);
+	Set<Integer> allVertices();
+	int getNumberOfVertices();
+	int getLength();
+	
+}
